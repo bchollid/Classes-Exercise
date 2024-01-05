69998dd142826afdd6687d736a6a5a3379693ee6
bchollid <brandonholliday1@gmail.com>
2024-01-05 11:49:50 -0700

    Finished

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var x5 = new Car();
+
+            x5.Make = "BMW";
+            x5.Model = "X5 Plug-In Hybrid";
+
+            Console.WriteLine($"The new car is a {x5.Make} {x5.Model}");
         }
     }
 }
+
+//        In the Car class:
+//Create a Make property of type string that is public
+//Create a Model property of type string that is public
+//Create a Year property of type int that is public
+//In the Main method:
+//Create a new instance of the Car class
+//Set values in the properties for the object
+//Print the values of each property to the Console
